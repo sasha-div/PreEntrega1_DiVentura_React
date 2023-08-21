aac2aa7079fa9331216f953b1f30b9da493b7244
Sasha Di Ventura <diventura.sasha@gmail.com>
2023-08-20 23:03:55 -0400

    Incluyendo componentes

---
+++ src/components/CartWidget/indexCartWidget.js
@@ -0,0 +1,9 @@
+import { CartFill } from 'react-bootstrap-icons';
+
+const CartWidget = () => {
+    return (
+        <CartFill size={20} />
+    );
+}
+
+export default CartWidget;
--- src/components/NavBar/indexNavBar.js
+++ src/components/NavBar/indexNavBar.js
@@ -2,21 +2,23 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from '../CartWidget/indexCartWidget';
+// import CartWidget from '../CartWidget/indexCartWidget';
 
 const NavBar = () => {
     return (
-        <>
+        <div>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#home">Incio</Nav.Link>
+                        <Nav.Link href="#features">Productos</Nav.Link>
+                        <Nav.Link href="#pricing"><CartWidget /></Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
-        </>
+        </div>
     )
 }
 
